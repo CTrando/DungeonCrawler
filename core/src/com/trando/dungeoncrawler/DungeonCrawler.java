b7279aef7410d20f576995b229c41cf56a83f303
CTrando <cameron.trando@yahoo.com>
2017-03-08 20:58:50 -0800

    Fixing PPM measures

--- core/src/com/trando/dungeoncrawler/DungeonCrawler.java
+++ core/src/com/trando/dungeoncrawler/DungeonCrawler.java
@@ -1,22 +1,23 @@
 package com.trando.dungeoncrawler;
 
-import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.viewport.*;
-import com.sun.prism.image.ViewPort;
 
 public class DungeonCrawler extends ApplicationAdapter implements InputProcessor {
     public static final float PPM = 16;
     Array<Test> bodies;
 
+    Body floor;
+
+    Box2DDebugRenderer renderer;
+
     OrthographicCamera camera;
     World world;
-    Vector2 gravity = new Vector2(0, 0);
+    Vector2 gravity = new Vector2(0, -9.81f);
     SpriteBatch batch;
 
     Body body;
@@ -24,43 +25,57 @@ public class DungeonCrawler extends ApplicationAdapter implements InputProcessor
 
     @Override
     public void create() {
+        Box2D.init();
         img = new Sprite(new Texture("badlogic.jpg"));
         bodies = new Array<Test>();
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera = new OrthographicCamera(Gdx.graphics.getWidth()/PPM, Gdx.graphics.getHeight()/PPM);
 
         world = new World(gravity, true);
+        renderer = new Box2DDebugRenderer();
 
         body = new Test(world).body;
+        floor = new Floor(world).body;
 
         for(int i = 0; i< 10; i++){
-            bodies.add(new Test(world));
+            //bodies.add(new Test(world));
         }
 
         batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this);
     }
 
-    int terrible = 0;
     @Override
     public void render() {
         System.out.println(body.getPosition().toString());
         batch.setProjectionMatrix(camera.combined);
-        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
         img.setPosition(body.getPosition().x, body.getPosition().y);
-        camera.position.set(body.getPosition().x + 100, body.getPosition().y + 100, 0);
+        float x = body.getPosition().x;
+        float y = body.getPosition().y;
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        camera.position.set(x , y , 0);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render(world, camera.combined);
         batch.begin();
-        batch.draw(img, body.getPosition().x, body.getPosition().y);
+        batch.draw(img, body.getPosition().x - (img.getWidth()/PPM)/2, body.getPosition().y - (img.getHeight()/PPM)/2, img.getWidth()/PPM, img.getHeight()/PPM);
         for(Test test: bodies){
             Vector2 vec = new Vector2();
             vec.setToRandomDirection();
-            batch.draw(test.img, test.body.getPosition().x, test.body.getPosition().y);
+
             test.body.applyLinearImpulse(vec, test.body.getPosition(), true);
+            //batch.draw(test.img, test.body.getPosition().x, test.body.getPosition().y, img.getWidth()/PPM, img.getHeight()/PPM);
         }
         batch.end();
         camera.update();
+
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+    }
+
+    @Override
+    public void resize(int width, int height){
+        camera.setToOrtho(false, width/PPM, height/PPM);
     }
 
     @Override
@@ -76,6 +91,12 @@ public class DungeonCrawler extends ApplicationAdapter implements InputProcessor
                 break;
             case Input.Keys.DOWN:
                 body.applyLinearImpulse(new Vector2(0, -5f), new Vector2(0, 0), true);
+                break;
+            case Input.Keys.RIGHT:
+                body.applyLinearImpulse(new Vector2(5f, 0),new Vector2(0, 0), true);
+                break;
+            case Input.Keys.LEFT:
+                body.applyLinearImpulse(new Vector2(-5f, 0),new Vector2(0, 0), true);
                 break;
             case Input.Keys.A:
                 //doesn't have rotation fixture so cannot rotate
--- core/src/com/trando/dungeoncrawler/Floor.java
+++ core/src/com/trando/dungeoncrawler/Floor.java
@@ -3,33 +3,27 @@ package com.trando.dungeoncrawler;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import static com.trando.dungeoncrawler.DungeonCrawler.PPM;
 
 /**
- * Created by Cameron on 3/7/2017.
+ * Created by Cameron on 3/8/2017.
  */
-public class Test {
+public class Floor {
     public Body body;
-    Sprite img;
 
-
-    public Test(World world) {
-        img = new Sprite(new Texture("badlogic.jpg"));
-
+    public Floor(World world){
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(img.getX(), img.getY());
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(0, -20);
         body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(img.getWidth() / 2/ PPM, img.getHeight() /2
-                /PPM);
+        shape.setAsBox(32 / 2, 32 /2);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
-        fixtureDef.density = 0.001f;
+        fixtureDef.density = 0.1f;
         fixtureDef.restitution = 0.5f;
 
         body.createFixture(fixtureDef);
